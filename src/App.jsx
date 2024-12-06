821cb19fdeca50e1437318b1adc3cf4a04969f10
Alex Teper <ateper@fi.uba.ar>
2024-12-05 21:00:04 -0300

    feat: add Features component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import About from './components/About.jsx';
+import Features from './components/Features.jsx';
+
 import Hero from './components/Hero.jsx';
 import NavBar from './components/Navbar.jsx';
 
@@ -8,6 +10,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
