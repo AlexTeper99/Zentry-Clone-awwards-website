bda40efd886b160d381efc24d8f8fd6f26beb126
Alex Teper <ateper@fi.uba.ar>
2024-12-10 17:25:03 -0300

    refactor: remove unused VideoPreview component and adjust timeout duration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,6 @@ import { TiLocationArrow } from 'react-icons/ti';
 import { useEffect, useRef, useState } from 'react';
 
 import Button from './Button';
-import VideoPreview from './VideoPreview';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -60,7 +59,7 @@ const Hero = () => {
 
         setTimeout(() => {
           setcurrentIndexBackup(currentIndex);
-        }, '1000');
+        }, '1500');
       }
     },
     {
@@ -126,22 +125,20 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <VideoPreview>
-              <div
-                onClick={handleMiniVdClick}
-                className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-              >
-                <video
-                  ref={nextVdRef}
-                  src={getVideoSrc((currentIndex % totalVideos) + 1)}
-                  loop
-                  muted
-                  id="current-video"
-                  className="size-64 origin-center scale-150 object-cover object-center"
-                  onLoadedData={handleVideoLoad}
-                />
-              </div>
-            </VideoPreview>
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVdRef}
+                src={getVideoSrc((currentIndex % totalVideos) + 1)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
           </div>
 
           <video
